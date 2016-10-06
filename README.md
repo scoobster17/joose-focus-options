17d4a25e6ba5893033279b36539ef346e9ac6821
Phil Gibbins <scoobster17@hotmail.co.uk>
2016-10-06 12:09:53 +0100

    Just committing everything I have so far

---
+++ README.md
@@ -0,0 +1,6 @@
+# Joose Focus Options
+
+This repo so far is for JavaScript training purposes to experiment with creating
+an area which expands on hover without CSS transitions (IE9 friendly).
+
+At a later time it may / will be added to the Joose Component Library.
--- src/js/joose-focus-options.js
+++ src/js/joose-focus-options.js
@@ -8,14 +8,33 @@ joose.focusOptions = (function(joose) {
     // shared configuration / variables
     var config = {
         focusType: 'expand',
-        direction: 'horizontal'
+        direction: 'horizontal',
+        focusedClass: 'focused',
+        notFocusedClass: 'not-focused'
     };
 
     // private functions
     var init = function () {
         var focusOptionsAreas = document.querySelectorAll('[data-component="focus-options"]');
         focusOptionsAreas.forEach(function(focusOptionsArea) {
-            console.log(focusOptionsArea);
+
+            var focusableOptions = focusOptionsArea.children;
+            var noOfOptions = focusableOptions.length;
+
+            for (var i=0; i<noOfOptions; i++) {
+
+                focusableOption = focusableOptions[i];
+
+                focusableOption.addEventListener('mouseover', function(event) {
+
+                    focusableOptions.forEach(function(option) {
+                        //joose.utils.removeClass(option, config.focusedClass);
+                        //joose.utils.addClass(event.target, config.focusedClass);
+                        console.log(option == event.target)
+                    });
+
+                });
+            }
         });
     }
 
